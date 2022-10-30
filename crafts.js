41f314e566814e96c68567432d5a54dd365e8c22
7DFA1 <117045579+7DFA1@users.noreply.github.com>
2022-10-31 00:06:14 +0200

    Add files via upload

---
+++ crafts.js
@@ -0,0 +1,10 @@
+export const crafts = [
+    {
+        "slots": [
+            "stiff_leather","stiff_leather","stiff_leather",
+            "stiff_leather","air","stiff_leather",
+            "air","air","air",
+            "meta_helmet"
+        ]
+    },
+]
---
+++ items.js
@@ -0,0 +1,5 @@
+export const items = [
+    {"id":"air", "title":"Air", "imgPath":"", "source":""}, 
+    {"id":"stiff_leather", "title":"Stiff leather", "imgPath":"items/leather.png", "source":""}, 
+    {"id":"meta_helmet", "title":"Helmet for test", "imgPath": "items/t-helmet.png", "source":""}
+]
